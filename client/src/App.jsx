4089e1f56fa5c5b7c3d9321a4adf5f9d8965c587
Vishal1702 <wizy100@gmail.com>
2022-02-23 10:36:43 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  Navbar,
+  Welcome,
+  Footer,
+  Services,
+  Transactions,
+} from "./components/index";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
